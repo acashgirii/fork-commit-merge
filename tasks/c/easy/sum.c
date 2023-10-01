f9cffadc53477f1a4204e4f4489f6aa59341191b
aakash <aakashgirisc01@gmail.com>
2023-10-01 19:11:26 +0530

    Fixed issue

--- tasks/c/easy/sum.c
+++ tasks/c/easy/sum.c
@@ -3,9 +3,19 @@
 #include <stdio.h>
 
 // TODO: Implement the sum function
+int sum(int a, int b)
+{
+    int sum = a+b;
+    printf("The sum is: %d", sum);
+}
 
 int main() {
     // TODO: Implement the main function
+    int a, b;
+    printf("Enter the two numbers: ");
+    scanf("%d%d",&a, &b);
 
+    sum(a,b);
+    
     return 0;
 }
